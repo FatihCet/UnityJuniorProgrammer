13158b6e1ba8ef89d45deaf61757c307ba2acde4
FatihCETİNER <fatihcetiner74@gmail.com>
2021-10-08 19:10:30 +0300

    Player Scripts
    Player motion system

---
+++ Prototype2/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10.0f;
+    public GameObject projectilePrefabs;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            Instantiate(projectilePrefabs, transform.position, transform.rotation);
+
+        }
+
+
+    }
+}
