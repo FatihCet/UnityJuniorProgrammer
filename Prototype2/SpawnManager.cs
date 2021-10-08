97b6ba95d4a2e8fcf25e720ad2166815efd30417
FatihCETİNER <fatihcetiner74@gmail.com>
2021-10-08 19:09:49 +0300

    Spawn manager Scripts
    I created spawn

---
+++ Prototype2/SpawnManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    public GameObject[] animalPrefabs;
+    public float spawnRange = 20;
+    public float spawnPosZ = -20;
+    
+
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+
+    
+    void Start()
+    {
+        InvokeRepeating(nameof(SpawnRandomAnimal), startDelay, spawnInterval);
+
+    }
+
+    void Update()
+    {
+
+
+
+
+
+    }
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRange, spawnRange), 0, spawnPosZ);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+
+    }
+}
